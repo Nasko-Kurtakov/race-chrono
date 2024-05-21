27d598522c621377701e770dbee910b81c77bff1
Mihail Kirilov <hidr0.frbg@gmail.com>
2024-05-21 22:20:45 +0300

    Made a lap table with upto 4 teams. Currently it works with only two teams as there is a bug where when the faster team laps us in the track the values are wrong.
    It is right for the first 4 laps

--- client/index.js
+++ client/index.js
@@ -10,22 +10,49 @@ ws.onopen = () => {
   console.log("Connected to the WebSocket server");
 };
 
+obj = {}
+delta = {}
+
+ourTeam = "0";
 ws.onmessage = (event) => {
   const data = JSON.parse(event.data);
-
   const { lap, driver, lapTime } = data;
 
-  if (driver === "Gaidov") {
-    currentLap = lap;
-    winnerLapTime = lapTime;
+  obj[driver] ||= { laps: [], avg: undefined, crossings: []}
 
-    writeDownLapRecord(lap, lapTime, "Gaidov");
-  } else if (driver === "Nasko" && currentLap === lap) {
-    writeDownLapRecord(lap, lapTime, "Nasko");
-
-    const gap = (winnerLapTime - lapTime).toFixed(3);
-    writeDownGap(lap, gap);
+  obj[driver].laps.push(lapTime)
+  obj[driver].crossings.push(new Date() / 1)
+  obj[driver].lastLap = lap;
+  if (obj[driver].laps.length > 3){
+    obj[driver].laps.shift()
+    obj[driver].crossings.shift()
   }
+
+  // console.log(obj);
+  teams = Object.keys(obj);
+  teams.forEach(team => {
+    teamLaps = obj[team].laps;
+
+    if (teamLaps) {
+      obj[team].avg = teamLaps.reduce((total, num) => total + num, 0) / teamLaps.length;
+    }
+    if (team !== ourTeam) {
+      delta[team] ||= {}
+
+      teamCrossing = obj[team].crossings[obj[team].crossings.length - 1]
+      ourTeamCrossing = obj[ourTeam].crossings[obj[team].crossings.length - 1]
+      delta[team]["last"] =  teamCrossing - ourTeamCrossing
+      gosho(team, delta[team]["last"], obj[team].laps[obj[team].laps.length - 1] - obj[ourTeam].laps[obj[ourTeam].laps.length - 1], 0);
+    }
+
+  })
+  console.log(data);
+
+
+  debugger;
+  // console.log(delta)
+  // console.log(teams)
+
 };
 
 ws.onclose = () => {
@@ -50,3 +77,14 @@ const writeDownGap = (lap, gap) => {
   gapElement.textContent = `Lap ${lap} - Gap: ${gap}s`;
   gaps.appendChild(gapElement);
 };
+
+const gosho = (teamId, delta, lastLap, lastThreeLaps) => {
+  const teamRow = document.getElementById(`${teamId}`);
+  // Name
+  teamRow.children[0].innerText = teamId;
+
+  teamRow.children[1].innerText = lastLap;
+  teamRow.children[2].innerText = lastThreeLaps;
+  teamRow.children[3].innerText = delta;
+
+}
--- server/server.js
+++ server/server.js
@@ -4,79 +4,47 @@ const WS_PORT = 8080;
 const wss = new WebSocket.Server({ port: WS_PORT });
 
 const START_GAP = 15; // in seconds
-const LAP_COUNT = 10;
-
+const LAP_COUNT = 1000;
+const COMPETITORS = 2;
 // Function to generate a random lap time between min and max seconds
 function generateLapTime(min, max) {
   return +(Math.random() * (max - min) + min).toFixed(3);
 }
 
-const winnerTimes = Array.from({ length: LAP_COUNT }, () =>
-  generateLapTime(50, 52)
-);
-const loserTimes = Array.from({ length: LAP_COUNT }, () =>
-  generateLapTime(51, 53)
-);
-
 wss.on("connection", (ws) => {
   console.log("New client connected");
 
-  let lapIndex = 0;
+  const sendLapTimes = (lap, teamId) => {
+    console.log("in sendLapTimes");
+    const data = {
+      lap,
+      driver: teamId,
+      lapTime: teamId == 0 ? 4 : 5,
+    };
+    console.log(data.lapTime)
 
-  const sendLapTimes = () => {
-    if (lapIndex < LAP_COUNT) {
-      const winnerLapTime = winnerTimes[lapIndex];
-      const loserLapTime = loserTimes[lapIndex];
-      const lap = lapIndex + 1;
-      const gapInSeconds = Math.abs(winnerLapTime - loserLapTime);
+    setTimeout(() => {
+      sendData(ws, JSON.stringify(data));
+      sendLapTimes(lap + 1, teamId)
+    }, data.lapTime * 1000);
 
-      // Gaidov's lap time
-      const winnerData = {
-        lap,
-        driver: "Gaidov",
-        lapTime: winnerLapTime,
-      };
+  }
 
-      sendData(ws, JSON.stringify(winnerData));
-
-      // Nasko's lap time after the calculated gap
-      setTimeout(() => {
-        const loserData = {
-          lap,
-          driver: "Nasko",
-          lapTime: loserLapTime,
-        };
-
-        sendData(ws, JSON.stringify(loserData));
-        verboseLog(gapInSeconds);
-        lapIndex += 1;
-
-        // Schedule the next lap times to be sent
-        if (lapIndex < LAP_COUNT) {
-          setTimeout(sendLapTimes, 3000); // Wait before starting the next lap for simplicity
-        } else {
-          verboseLog("Race completed");
-          ws.close();
-        }
-      }, gapInSeconds * 1000 + (lapIndex == 0 ? START_GAP * 1000 : 0)); // Delay based on the lap time gap
-    }
-  };
-
-  // Start sending lap times
-  sendLapTimes();
-
-  // Handle client disconnection
-  ws.on("close", () => {
-    console.log("Client disconnected");
-    clearInterval(intervalId);
-  });
+  for (let index = 0; index < COMPETITORS; index++) {
+    sendLapTimes(1, index);
+  }
 });
+
+// Handle client disconnection
+// ws.on("close", () => {
+//   console.log("Client disconnected");
+//   clearInterval(intervalId);
+// });
 
 console.log(`WebSocket server is running on ws://localhost:${WS_PORT}`);
 
-const verboseLog = () => console.log(...arguments);
 
 const sendData = (ws, data) => {
-  verboseLog(data);
+  // verboseLog(data);
   ws.send(data);
 };
